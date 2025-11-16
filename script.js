1ee00a9ce58b9919212b1626af988282cdd3ef9f
Aefrly <fraley.allison@ic.edu>
2025-11-16 11:56:30 -0600

    Added a slideshow of images in HTML, styled in CSS, and edited information in JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+//Double check that the script is linked properly
 console.log("Hello World!");
+
+//JavaScript Code for slideshow
+let slideIndex = 1;
+showSlide(slideIndex);
+
+function changeSlide(n) {
+    showSlide(slideIndex += n);
+}
+
+function showSlide(n) {
+    const slides = document.querySelectorAll('.slide');
+    if (n > slides.length) { slideIndex = 1; }
+    if (n < 1) { slideIndex = slides.length; }
+    slides.forEach(slide => slide.classList.remove('active'));
+    slides[slideIndex - 1].classList.add('active');
+}
