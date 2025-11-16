d2b109e21a5b68d43b43359d96299fa4ba1887ab
Aefrly <fraley.allison@ic.edu>
2025-11-16 14:28:01 -0600

    Attempting to retrieve weather information

--- script.js
+++ script.js
@@ -19,3 +19,33 @@ function showSlide(n) {
     activeSlide.classList.remove('fade');
     void activeSlide.offsetWidth; // Trigger reflow
 }
+
+//JavaScript for API on weather information 
+//Sending API requests using async/await
+async function getWeatherInfo() {
+  const response = await fetch("https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid=89be7347dc28111fd85e4f4987994e48");
+  const data = await response.json();
+  
+  //Retrieve data from API
+  const temp = data.current.temp;
+  const description = data.current.weather[0].description;
+  const humidity = data.current.humidity;
+  const windSpeed = data.current.wind_speed;
+
+  // Update the DOM with weather info
+    const output = document.getElementById("weather-container"); 
+    output.innerHTML = `
+      <div class="col-12 text-center">
+        <h3>Jacksonville, IL Weather</h3>
+        <p><strong>Temperature:</strong> ${temp}°C</p>
+        <p><strong>Condition:</strong> ${description}</p>
+        <p><strong>Humidity:</strong> ${humidity}%</p>
+        <p><strong>Wind Speed:</strong> ${windSpeed} m/s</p>
+      </div>
+    `;
+}
+
+const button = document.getElementById('fetch-weather-btn');
+button.addEventListener('click', function() {
+    getWeatherInfo();
+});
